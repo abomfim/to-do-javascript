230a598f70f12986e06102d70d4fc0b251d2b9d3
Allan Bomfim <bomfim.allan@gmail.com>
2020-03-28 18:35:04 -0300

    initial commit

---
+++ todo.js
@@ -0,0 +1,45 @@
+var ulElement = document.querySelector("#app ul");
+var inputElement = document.querySelector("#app input");
+var buttonElement = document.querySelector("#app button");
+
+var tasks = JSON.parse(localStorage.getItem('taskList')) || [];
+
+function render() {
+    ulElement.innerHTML = '';
+    for(task of tasks) {
+        var taskElement = document.createElement('li');
+        var tastText = document.createTextNode(task);
+
+        var linkElement = document.createElement('a');
+        linkElement.setAttribute('href', '#');
+        linkElement.setAttribute('onclick', 'exclude('+tasks.indexOf(task)+')');
+        
+        var linkText = document.createTextNode(' Excluir');
+        linkElement.appendChild(linkText);
+        
+        taskElement.appendChild(tastText);
+        taskElement.appendChild(linkElement);
+        ulElement.appendChild(taskElement);
+    }
+}
+
+render();
+
+function add(){
+    tasks.push(inputElement.value);
+    inputElement.value = '';
+    render();
+    saveToStorage();
+}
+
+buttonElement.onclick = add;
+
+function exclude(taskPosition) {
+    tasks.splice(taskPosition, 1);
+    render();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('taskList', JSON.stringify(tasks));
+}
